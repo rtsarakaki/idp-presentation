83fc8004872189f92634b545420720c01b16e3ea
rtsarakaki <rtsarakaki@gmail.com>
2025-08-04 10:32:59 -0300

    fix: correct email to rtsarakaki@gmail.com

--- src/data/presentation-outline.ts
+++ src/data/presentation-outline.ts
@@ -163,7 +163,7 @@ export const presentationOutline: IPresentationOutline = {
               subtitle: "Perguntas e Discussão",
               content: [
                 "Ricardo Tadeu Sinhei Arakaki",
-                "ricardo.arakaki@gmail.com",
+                "rtsarakaki@gmail.com",
                 "linkedin.com/in/ricardo-arakaki",
                 "github.com/rtsarakaki"
               ],
